dbd096be5247663b79ea0da2e02f971bb285f06e
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 10:36:48 -0400

    add donations, expansion project, and services page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./pages/Home";
 import RootLayout from "./components/layout/RootLayout";
+import Donations from "./pages/Donations";
+import Services from "./pages/Services";
+import ExpansionProject from "./pages/ExpansionProject";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/donate" element={<Donations />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/expansion" element={<ExpansionProject />} />
         </Route>
       </Routes>
     </Router>
