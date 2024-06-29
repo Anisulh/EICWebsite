1d6dcd6846c4598ed50e78ea67090d778a9e3cab
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 19:48:35 -0400

    remove max width

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <div className="max-w-4xl mx-auto mt-10">
+    <div className="mt-10">
       <Outlet />
     </div>
   );
