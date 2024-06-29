baa1d9dfe8e95244815ae8cd11f42515791076bd
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 18:32:36 -0400

    initial counseling page

---
+++ src/pages/Counseling.tsx
@@ -0,0 +1,35 @@
+export default function Counseling() {
+  return (
+    <div className="text-xl">
+      <h1 className="text-2xl font-bold text-center w-full mb-10">
+        Counseling Service
+      </h1>
+      <p>
+        Our Shara'ee counseling services address various aspects of Muslim life:
+      </p>
+      <ol className="list-decimal max-w-3xl mx-auto my-8 space-y-4">
+        <li>
+          <b>Family Counseling:</b> Addressing intergenerational conflicts,
+          parenting challenges, and family dynamics.
+        </li>
+        <li>
+          <b>Marriage Counseling:</b>Helping couples navigate marital issues in
+          line with Islamic principles.
+        </li>
+        <li>
+          <b> Youth Counseling:</b> Guiding young Muslims through challenges
+          unique to their age group.
+        </li>
+        <li>
+          <b> Spiritual Counseling:</b>Assisting individuals in strengthening
+          their faith and overcoming spiritual obstacles.
+        </li>
+      </ol>
+      <p>
+        Our counselors are well-versed in Islamic teachings and modern
+        counseling techniques, offering a balanced approach to addressing life's
+        challenges.
+      </p>
+    </div>
+  );
+}
