61d9e6eb7e031e1159b70f4e36d7fb26c767c8d7
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 18:31:56 -0400

    add services sub pages to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import NavBar from "./components/navbar/NavBar";
 import Home from "./pages/Home";
 import RootLayout from "./components/layout/RootLayout";
 import Donations from "./pages/Donations";
-import Services from "./pages/Services";
 import ExpansionProject from "./pages/ExpansionProject";
+import Nikah from "./pages/Nikah";
+import Counseling from "./pages/Counseling";
+import SocialRelief from "./pages/SocialRelief";
+import WeekendSchool from "./pages/WeekendSchool";
 
 function App() {
   return (
@@ -14,8 +17,11 @@ function App() {
         <Route element={<RootLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/donate" element={<Donations />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/expansion" element={<ExpansionProject />} />
+          <Route path="/nikah" element={<Nikah />} />
+          <Route path="/counseling" element={<Counseling />} />
+          <Route path="/social-relief" element={<SocialRelief />} />
+          <Route path="/weekend-school" element={<WeekendSchool />} />
         </Route>
       </Routes>
     </Router>
