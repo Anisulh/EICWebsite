c9aa2032c6705d6411ac8f94718aaac37ccaedfb
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 18:32:14 -0400

    initial socialRelief page

---
+++ src/pages/SocialRelief.tsx
@@ -0,0 +1,27 @@
+export default function SocialRelief() {
+  return (
+    <div className="text-xl">
+      <h1 className="text-2xl font-bold text-center w-full mb-10">
+        Relief Work & Social Services
+      </h1>
+      <p>
+        Our community is committed to supporting those in need, both locally and
+        globally. Our relief efforts include:
+      </p>
+      <ol className="list-decimal max-w-3xl mx-auto my-8 space-y-4">
+        <li>
+          <b>Rohingya Crisis Support:</b> We've organized fundraising campaigns
+          for flood victims to aid Rohingya refugees.
+        </li>
+        <li>
+          <b>Palestine Refugee Support:</b> Ongoing efforts to fundraise aid for
+          Palestinian refugees.
+        </li>
+      </ol>
+      <p>
+        We believe in the power of community action and welcome volunteers to
+        join our efforts.
+      </p>
+    </div>
+  );
+}
