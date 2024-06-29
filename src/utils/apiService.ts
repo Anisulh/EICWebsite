fe341a0fd6965459aa811d3e807f0ca1d584890d
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 19:14:04 -0400

    fix api issue showing mix blocked content due to http request instead of https

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://api.aladhan.com/v1';
+const API_BASE_URL = 'https://api.aladhan.com/v1';
 const API_METHOD = 2; // Example: Islamic Society of North America (ISNA)
 const CITY = 'Elmhurst';
 const COUNTRY = 'United States';
