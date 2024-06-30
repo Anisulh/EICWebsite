79ef274b5718623302ae3f2ea3dcb03dc2eb8aaa
ubergoose <anisulhaque9391@gmail.com>
2024-06-30 12:21:11 -0400

    add additional styling

--- src/components/ui/DropDown.tsx
+++ src/components/ui/DropDown.tsx
@@ -15,19 +15,19 @@ export default function DropDown({
 }) {
   return (
     <Menu>
-      <MenuButton className="inline-flex gap-1 items-center hover:bg-teal-600 px-3 py-2 rounded-md transition-colors">
+      <MenuButton className="inline-flex gap-1 items-center hover:bg-teal-600 px-3 py-2 rounded-md transition-colors w-full md:ml-4 font-bold">
         {label}
-        <ChevronDownIcon className="size-4" />
+        <ChevronDownIcon className="size-6" />
       </MenuButton>
       <MenuItems
         transition
-        anchor="bottom end"
-        className="w-52 origin-top-right rounded-xl border bg-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 "
+        anchor="bottom start"
+        className="w-52 origin-top-right rounded-xl border bg-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 shadow-md "
       >
         {menuItems.map((item) => (
           <MenuItem key={item.label}>
             <a
-              className="block transition-colors data-[focus]:bg-blue-100 px-4 py-2"
+              className="block transition-colors data-[focus]:bg-teal-600 data-[focus]:text-white px-4 py-2"
               href={item.link}
             >
               {item.label}
